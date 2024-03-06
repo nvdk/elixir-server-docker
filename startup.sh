99166b77c162987e4ec752c23e9363bc4a1e62a4
Niels Vandekeybus <progster@gmail.com>
2024-03-06 14:41:37 +0100

    remove bashisms from sh script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ "$DEVELOPMENT" != "" ]]
+if [ "$DEVELOPMENT" != "" ]
 then
     sh /setup.sh
 fi
@@ -10,25 +10,25 @@ CMD_OPTS=""
 ERL_OPTS=""
 ERL_OPTS="-start_epmd true"
 
-if [[ $IEX_NAME ]]
+if [ $IEX_NAME ]
 then
     NODE_NAME=`echo "$IEX_NAME" | sed -e "s/@ip/@$IP_ADDRESS/"`
     CMD_OPTS="$CMD_OPTS --name $NODE_NAME"
 fi
 
-if [[ $IEX_COOKIE ]]
+if [ $IEX_COOKIE ]
 then
     CMD_OPTS="$CMD_OPTS --cookie $IEX_COOKIE"
 fi
 
-if [[ $IEX_PORT ]]
+if [ $IEX_PORT ]
 then
     ERL_OPTS="$ERL_OPTS -kernel inet_dist_listen_min $IEX_PORT -kernel inet_dist_listen_max $IEX_PORT"
 fi
 
 cd /app
 
-if [[ ! -z "${LOG_ELIXIR_STARTUP_COMMAND}" ]]
+if [ ! -z "${LOG_ELIXIR_STARTUP_COMMAND}" ]
 then
   echo -n "Launching Elixir on IP $IP_ADDRESS as "
   echo "MIX_ENV=prod elixir $CMD_OPTS --erl \"$ERL_OPTS\" -pa _build/prod/consolidated -S mix run --no-halt"
